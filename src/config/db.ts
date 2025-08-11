9981fda3bae24ea7ba2013e99b4e51b1be9f3681
Joseph Hansel Daiz <joseph.hansel.daiz@gmail.com>
2025-08-12 07:24:18 +0800

    Update db config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,7 +13,14 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+        ca: fs.readFileSync(path.join(__dirname, '..', '..', 'certs', 'ap-southeast-2-bundle.pem')), // Corrected path
+      },
+    },
   }
 );
 
